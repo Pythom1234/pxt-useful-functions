aaf0ba29e3e3738cc6c2aeba8d52c2c374428d7a
Pythom1234 <149593207+Pythom1234@users.noreply.github.com>
2024-03-17 08:51:45 +0100

    Aktualizovat main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 //% icon="\uf0c9" block="Useful" block.loc.cs="Užitečné" color=#189851
 namespace useful {
-    //% block="comment $c" block.loc.cs="komentář $c" color=#d3d3d3
+    //% block="comment $c" block.loc.cs="komentář $c" color=#939393
     export function comment(c: string) {}
 }
