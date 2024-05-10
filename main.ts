7d5b88dd8251e0b868ea8d8311b7b9849618478d
Pythom1234 <149593207+Pythom1234@users.noreply.github.com>
2024-05-10 18:36:07 +0200

    Aktualizovat main.ts

--- main.ts
+++ main.ts
@@ -9,7 +9,7 @@ namespace useful {
     //% block="comment $c" block.loc.cs="komentář $c" color=#707070 weight=98
     export function commentBlocks(c: string, a: Action) { }
     //% block="in background" block.loc.cs="na pozadí" weight=97 handlerStatement
-    function inBackground(a: Action) {
+    export function inBackground(a: Action) {
         control.inBackground(a)
     }
 }
