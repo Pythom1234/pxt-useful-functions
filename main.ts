88012a9e35bef8f6b31c4ec605870a1e7e310445
Pythom1234 <149593207+Pythom1234@users.noreply.github.com>
2024-05-10 18:35:46 +0200

    Aktualizovat main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 //% icon="\uf0c9" block="Useful" block.loc.cs="Užitečné" color=#189851
 namespace useful {
-    //% block="comment $c" block.loc.cs="komentář $c" color=#707070 weight=100 subcategory="Comments" subcategory.loc.cs="Komentáře"
+    //% block="comment $c" block.loc.cs="komentář $c" color=#707070 weight=100
     export function comment(c: string) { }
     //% block="comment $c" block.loc.cs="komentář $c" color=#707070 weight=99 handlerStatement
     export function commentSection(c: string, a: Action) {
@@ -8,10 +8,8 @@ namespace useful {
     }
     //% block="comment $c" block.loc.cs="komentář $c" color=#707070 weight=98
     export function commentBlocks(c: string, a: Action) { }
-}
-namespace control {
-    //% block="in background" block.loc.cs="na pozadí" weight=99 handlerStatement
-    function inBackgroundInline(a: Action) {
+    //% block="in background" block.loc.cs="na pozadí" weight=97 handlerStatement
+    function inBackground(a: Action) {
         control.inBackground(a)
     }
 }
