bf8161c36c3973d84e4fd79bb6133cf4d9b28394
Pythom1234 <149593207+Pythom1234@users.noreply.github.com>
2024-05-10 18:51:41 +0200

    Aktualizovat main.ts

--- main.ts
+++ main.ts
@@ -2,8 +2,8 @@
 namespace useful {
     //% block="comment $c" block.loc.cs="komentář $c" color=#707070 weight=100
     export function comment(c: string) { }
-    //% block="comment $c||(run code $run)" block.loc.cs="komentář $c||(spouštět kód $run)" color=#707070 weight=99 handlerStatement run.shadow="toggleYesNo" expandableArgumentMode="enabled" run.defl=true
-    export function commentSection(c: string, run: boolean = true, a: Action) {
+    //% block="comment $c run $run" block.loc.cs="komentář $c spustit $run" color=#707070 weight=99 handlerStatement run.shadow="toggleYesNo" run.defl=true
+    export function commentSection(c: string, run: boolean, a: Action) {
         if (run) {
             a()
         }
